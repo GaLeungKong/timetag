2bf174aaa13517e7de5929818cc0cc9cdac252de
GaLeungKong <galeungkong@gmail.com>
2019-08-12 11:11:44 +0800

    regular commit

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
-import Index from './views/index.vue'
+import Index from './views/Index.vue'
 
 Vue.use(Router)
 
